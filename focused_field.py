e4b765229e4a3634ec388141f88ed06d73b048d8
SimpleCaci <edikus@outlook.es>
2025-11-25 18:03:44 -0800

    now it gets focused window and its position

--- focused_field.py
+++ focused_field.py
@@ -5,24 +5,20 @@ import time
 def get_focused_field_center():
     """Returns (x, y) of the center of the focused text field."""
     try:
-        focused = Desktop(backend="uia")
-        print(focused)
-        focused_windows  = focused.windows()
-        for window in focused_windows:
+        windows = Desktop(backend="uia").windows()
+        for window in windows:
             if window.is_active():
-                print(window)
-        #print(focused)
-        '''
-        if not focused:
+                focused_window = window
+
+        if not focused_window:
             return None
 
-        rect = focused.rectangle()
+        rect = focused_window.rectangle()
 
         center_x = (rect.left + rect.right) // 2
         center_y = (rect.top + rect.bottom) // 2
 
         return (center_x, center_y)
-        '''
 
     except Exception as e:
         print("UIA error:", e)
